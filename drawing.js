6e00486ab0c1be03e83fcae10a75b4c03963041a
WjahatAhmad <wajahat5ahmad@gmail.com>
2022-08-31 13:21:43 +0500

    animated asteriod

--- drawing.js
+++ drawing.js
@@ -1,11 +1,11 @@
-export const grid = (canvas, context, stroke = '#00FF00', fill = '#00AA00', minor = 10, major = 50, lineMin = 0.3, lineMax = 0.6) => {
+export const draw_grid = (context, stroke = '#00FF00', fill = '#00AA00', minor = 10, major = 50, lineMin = 0.3, lineMax = 0.6) => {
     context.save();
     context.strokeStyle = stroke;
     context.fillStyle = fill;
-    for (let x = 0; x < canvas.width; x += minor) {
+    for (let x = 0; x < context.canvas.width; x += minor) {
         context.beginPath();
         context.moveTo(x, 0);
-        context.lineTo(x, canvas.height);
+        context.lineTo(x, context.canvas.height);
         context.lineWidth = x % major === 0 ? lineMax : lineMin;
         if (x % major === 0) {
             context.fillText(`${x}`, x, 10);
@@ -13,10 +13,10 @@ export const grid = (canvas, context, stroke = '#00FF00', fill = '#00AA00', mino
         }
         context.stroke();
     }
-    for (let y = 0; y < canvas.height; y += minor) {
+    for (let y = 0; y < context.canvas.height; y += minor) {
         context.beginPath();
         context.moveTo(0, y);
-        context.lineTo(canvas.width, y);
+        context.lineTo(context.canvas.width, y);
         context.lineWidth = y % major === 0 ? lineMax : lineMin;
         if (y % major === 0) {
             context.fillText(`${y}`, 0, y + minor);
@@ -28,3 +28,29 @@ export const grid = (canvas, context, stroke = '#00FF00', fill = '#00AA00', mino
     context.restore();
 };
 
+
+
+export const draw_asteroid = (context, radius, shape, options) => {
+    options = options || { noise: 0.5 };
+    context.strokeStyle = options.stroke || "white";
+    context.fillStyle = options.fill || "black";
+    context.save();
+    context.beginPath();
+    for (let i = 0; i < shape.length; i++) {
+      context.rotate((2 * Math.PI) / shape.length);
+      context.lineTo(radius + radius * options.noise * shape[i], 0);
+    }
+    context.closePath();
+    context.fill();
+    context.stroke();
+    if (options.guide) {
+      context.lineWidth = 0.5;
+      context.beginPath();
+      context.arc(0, 0, radius + radius * options.noise, 0, 2 * Math.PI);
+      context.stroke();
+      context.beginPath();
+      context.arc(0, 0, radius - radius * options.noise, 0, 2 * Math.PI);
+      context.stroke();
+    }
+    context.restore();
+  };
